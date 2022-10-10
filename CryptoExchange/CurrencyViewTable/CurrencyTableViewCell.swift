898d6ea2f77eda914313fc485f217f3929128f97
a.gumen <a.gumen@revizto.com>
2022-10-10 12:59:38 +0200

    add ui elements and constraints to currency cell

--- CryptoExchange/CurrencyViewTable/CurrencyTableViewCell.swift
+++ CryptoExchange/CurrencyViewTable/CurrencyTableViewCell.swift
@@ -2,15 +2,26 @@ import UIKit
 
 final class CurrencyTableViewCell: UITableViewCell {
 
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var priceLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        // MARK: setup appearence
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    }
+    
+    // MARK: update data
+    
+    func updateData(name: String, price: Double) {
+        nameLabel.text = name
+        
+        let formattedPrice = String(format: "$%.2f", price)
+        priceLabel.text = formattedPrice
     }
     
 }
