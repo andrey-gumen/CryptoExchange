56b185bf7ecf24cbef65d2b0984783fcbe880c59
a.gumen <a.gumen@revizto.com>
2022-10-13 00:49:02 +0200

    add CurrencyDetailsViewController

---
+++ CryptoExchange/ViewControllers/CurrencyDetailsViewController.swift
@@ -0,0 +1,80 @@
+import UIKit
+
+final class CurrencyDetailsViewController: UIViewController {
+
+    private let titleLabel = UILabel()
+    private let priceLabel = UILabel()
+    
+    var currencyId: String = ""
+    
+    init(currencyId: String) {
+        self.currencyId = currencyId
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) is not supported")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupLayout()
+        setupAppearence()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        //print("request data for: \(currencyId)")
+        requestDetails()
+    }
+    
+    private func updateView(model: CryptoCurrencyModel?) {
+        titleLabel.text = model?.name ?? "Invalid data"
+        
+        let formattedPrice = model?.priceInUsd != nil ? String(format: "$%.2f", model!.priceInUsd!) : "-"
+        priceLabel.text = formattedPrice
+    }
+
+    private func requestDetails() {
+        APIManager.shared.getCurrencyDetails(ids: currencyId) { [weak self] error, models in
+            // in case of error array will be empty
+            self?.updateView(model: models.isEmpty ? nil : models[0])
+            
+            if let error = error {
+                print(error)
+            }
+        }
+    }
+}
+
+private extension CurrencyDetailsViewController {
+
+    // MARK: setup layout
+    func setupLayout() {
+        view.addSubview(titleLabel)
+        view.addSubview(priceLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 12).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 12).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12).isActive = true
+        priceLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12).isActive = true
+        priceLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 12).isActive = true
+        priceLabel.heightAnchor.constraint(equalToConstant: 24).isActive = true
+    }
+
+    // MARK: setup appearence
+    func setupAppearence() {
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 25.0)
+        priceLabel.font = UIFont.systemFont(ofSize: 18)
+    }
+    
+}
+
+
