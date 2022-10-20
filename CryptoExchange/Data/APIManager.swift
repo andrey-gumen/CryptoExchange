099713e0b5550980bcb5b4260d95b5ede342afb9
a.gumen <a.gumen@revizto.com>
2022-10-20 16:14:13 +0200

    add requests
    - to get assets icons
    - to download image

--- CryptoExchange/Data/APIManager.swift
+++ CryptoExchange/Data/APIManager.swift
@@ -1,4 +1,5 @@
 import Alamofire
+import UIKit
 
 final class APIManager {
     
@@ -16,10 +17,13 @@ final class APIManager {
     private enum EndPoints : String {
         case Exchanges = "/exchanges"
         case Assets = "/assets"
+        case Icons = "/assets/icons"
     }
     
     private var authorizationHeader: HTTPHeaders {
-        ["X-CoinAPI-Key": "BCC8F92D-F5D5-417C-B9F2-921893AFE609"]
+        ["X-CoinAPI-Key": "44116C84-40DE-4360-A06E-612DAC3C0205"]
+        //["X-CoinAPI-Key": "0481018D-3570-41A5-8C69-7F5AC9C64342"]
+        //["X-CoinAPI-Key": "BCC8F92D-F5D5-417C-B9F2-921893AFE609"]
     }
 
     // MARK: requests
@@ -28,8 +32,8 @@ final class APIManager {
             Constants.BaseUrl.rawValue + EndPoints.Assets.rawValue,
             method: .get,
             headers: authorizationHeader
-        ).responseDecodable(of: [CurrencyDescriptor].self) { data in
-            switch data.result {
+        ).responseDecodable(of: [CurrencyDescriptor].self) { response in
+            switch response.result {
             case .failure(let error): completion(error, [])
             case .success(let models): completion(nil, models)
             }
@@ -44,10 +48,36 @@ final class APIManager {
             method: .get,
             parameters: parameters,
             headers: authorizationHeader
-        ).responseDecodable(of: [CurrencyModel].self) { data in
-            switch data.result {
+        ).responseDecodable(of: [CurrencyModel].self) { response in
+            switch response.result {
             case .failure(let error): completion(error, [])
             case .success(let models): completion(nil, models)
+            }
+        }
+    }
+    
+    func getCurrencyIcon(id: String,  completion: @escaping ((Error?, [CurrencyIcon]) -> Void)) {
+        AF.request(
+            Constants.BaseUrl.rawValue + EndPoints.Icons.rawValue,
+            method: .get,
+            headers: authorizationHeader
+        ).responseDecodable(of: [CurrencyIcon].self) { response in
+            switch response.result {
+            case .failure(let error): completion(error, [])
+            case .success(let models): completion(nil, models)
+            }
+        }
+    }
+    
+    func downloadImage(url: String,  completion: @escaping ((Error?, UIImage?) -> Void)) {
+        AF.request(
+            url,
+            method: .get,
+            headers: authorizationHeader
+        ).response { response in
+            switch response.result {
+            case .failure(let error): completion(error, nil)
+            case .success(let data): completion(nil, UIImage(data: data!, scale: 1))
             }
         }
     }
