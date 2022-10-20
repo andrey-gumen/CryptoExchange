cb9c6dc2ebd1ab35f448f9e63a3adc9fc7e84b3f
a.gumen <a.gumen@revizto.com>
2022-10-20 15:53:39 +0200

    update navigation bar
    - add activity indicator to navigation bar
    - put currency title on navigation bar

--- CryptoExchange/ViewControllers/CurrencyDetailsViewController.swift
+++ CryptoExchange/ViewControllers/CurrencyDetailsViewController.swift
@@ -2,10 +2,23 @@ import UIKit
 
 final class CurrencyDetailsViewController: UIViewController {
 
-    private let titleLabel = UILabel()
+    // MARK: ui views
+    private let titleLabel = UILabel();
     private let priceLabel = UILabel()
+    private let activityIndicator = UIActivityIndicatorView(style: .medium)
     
-    var currencyId: String
+    // MARK: permanent data
+    private let currencyId: String
+    
+    var IsRequestInProgress: Bool = false {
+        didSet {
+            if IsRequestInProgress {
+                activityIndicator.startAnimating()
+            } else {
+                activityIndicator.stopAnimating()
+            }
+        }
+    }
     
     init(currencyId: String) {
         self.currencyId = currencyId
@@ -32,13 +45,17 @@ final class CurrencyDetailsViewController: UIViewController {
     
     private func updateView(model: CurrencyModel?) {
         titleLabel.text = model?.name ?? "Invalid data"
+        titleLabel.sizeToFit()
         
         let formattedPrice = model?.priceInUsd != nil ? String(format: "$%.2f", model!.priceInUsd!) : "-"
         priceLabel.text = formattedPrice
     }
 
     private func requestDetails() {
+        IsRequestInProgress = true
         APIManager.shared.getCurrencyDetails(ids: currencyId) { [weak self] error, models in
+            self?.IsRequestInProgress = false
+            
             // in case of error array will be empty
             self?.updateView(model: models.isEmpty ? nil : models[0])
             
@@ -53,21 +70,24 @@ private extension CurrencyDetailsViewController {
 
     // MARK: setup layout
     func setupLayout() {
-        view.addSubview(titleLabel)
         view.addSubview(priceLabel)
         
-        let layoutGuide = view.safeAreaLayoutGuide;
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.topAnchor.constraint(equalTo: layoutGuide.topAnchor, constant: 12).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor, constant: 12).isActive = true
-        titleLabel.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor, constant: 12).isActive = true
-        titleLabel.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        // navigation bar
+        navigationItem.titleView = titleLabel
         
+        let barButton = UIBarButtonItem(customView: activityIndicator)
+        barButton.customView?.sizeToFit()
+        navigationItem.setRightBarButton(barButton, animated: false)
+            
+        // content
+        let layoutGuide = view.safeAreaLayoutGuide;
         priceLabel.translatesAutoresizingMaskIntoConstraints = false
-        priceLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12).isActive = true
+        priceLabel.topAnchor.constraint(equalTo: layoutGuide.topAnchor, constant: 12).isActive = true
         priceLabel.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor, constant: 12).isActive = true
         priceLabel.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor, constant: 12).isActive = true
         priceLabel.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        
+        
     }
 
     // MARK: setup appearence
@@ -75,7 +95,10 @@ private extension CurrencyDetailsViewController {
         view.backgroundColor = UIColor(named: "viewBackground")
         
         titleLabel.font = UIFont.boldSystemFont(ofSize: 25.0)
+        titleLabel.textAlignment = .center
+        
         priceLabel.font = UIFont.systemFont(ofSize: 18)
+        activityIndicator.hidesWhenStopped = true
     }
     
 }
