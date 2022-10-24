767d41bdac8787790ba3c0eac080ad09e0a553f5
a.gumen <a.gumen@revizto.com>
2022-10-24 19:30:11 +0200

    add one more API key

--- CryptoExchange/Data/APIManager.swift
+++ CryptoExchange/Data/APIManager.swift
@@ -21,8 +21,8 @@ final class APIManager {
     }
     
     private var authorizationHeader: HTTPHeaders {
-        ["X-CoinAPI-Key": "44116C84-40DE-4360-A06E-612DAC3C0205"]
-        //["X-CoinAPI-Key": "0481018D-3570-41A5-8C69-7F5AC9C64342"]
+        //["X-CoinAPI-Key": "44116C84-40DE-4360-A06E-612DAC3C0205"]
+        ["X-CoinAPI-Key": "0481018D-3570-41A5-8C69-7F5AC9C64342"]
         //["X-CoinAPI-Key": "BCC8F92D-F5D5-417C-B9F2-921893AFE609"]
     }
 
@@ -42,7 +42,7 @@ final class APIManager {
        
     func getCurrencyDetails(ids: String,  completion: @escaping ((Error?, [CurrencyModel]) -> Void)) {
         let parameters: Parameters = ["filter_asset_id": ids]
-        
+
         AF.request(
             Constants.BaseUrl.rawValue + EndPoints.Assets.rawValue,
             method: .get,
