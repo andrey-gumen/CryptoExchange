ba2a6960591032c04d8ed3b2f4132814f8163daa
a.gumen <a.gumen@revizto.com>
2022-10-26 22:10:28 +0200

    add ColorSchemes

---
+++ CryptoExchange/ViewControllers/ColorScheme.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ColorScheme {
+
+    static let viewBackground = UIColor(named: "viewBackground")
+    static let currencyCellBackground = UIColor(named: "currencyCellBackground")
+    static let currencyTableViewBackground = UIColor(named: "currencyTableViewBackground")
+
+    
+}
