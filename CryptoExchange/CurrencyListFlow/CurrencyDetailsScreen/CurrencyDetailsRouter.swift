b973a3e8767e6da2fb8a5495aeeee1dd45219d51
a.gumen <a.gumen@revizto.com>
2022-10-30 22:46:04 +0100

    assemble MVP in CurrencyDetailsRouter

--- CryptoExchange/CurrencyListFlow/CurrencyDetailsScreen/CurrencyDetailsRouter.swift
+++ CryptoExchange/CurrencyListFlow/CurrencyDetailsScreen/CurrencyDetailsRouter.swift
@@ -8,8 +8,14 @@ final class CurrencyDetailsRouter {
     
     private let navigationController: UINavigationController!
     
-    init(navigationController: UINavigationController) {
+    init(navigationController: UINavigationController, currencyId: String) {
         self.navigationController = navigationController
+        
+        let view = DefaultCurrencyDetailsView()
+        let presenter = DefaultCurrencyDetailsPresenter(id: currencyId, view: view)
+        view.presenter = presenter
+        
+        navigationController.pushViewController(view, animated: true)
     }
     
     func moveBack() {
