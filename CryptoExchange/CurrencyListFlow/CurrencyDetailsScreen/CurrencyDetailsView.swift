451c93a27eb4992ab32ae7549b15ed6781d6cd55
a.gumen <a.gumen@revizto.com>
2022-10-30 22:40:24 +0100

    implement DefaultCurrencyDetailsView
    - move request to presenter
    - move currencyId to presenter
    - implement CurrencyDetailsView methods

--- CryptoExchange/CurrencyListFlow/CurrencyDetailsScreen/CurrencyDetailsView.swift
+++ CryptoExchange/CurrencyListFlow/CurrencyDetailsScreen/CurrencyDetailsView.swift
@@ -6,6 +6,8 @@ protocol CurrencyDetailsView: AnyObject {
 }
 
 final class DefaultCurrencyDetailsView: UIViewController {
+    
+    var presenter: CurrencyDetailsPresenter?
 
     // MARK: ui views
     private let titleLabel = UILabel();
@@ -13,9 +15,6 @@ final class DefaultCurrencyDetailsView: UIViewController {
     private let typeLabel = UILabel()
     private let priceLabel = UILabel()
     private let activityIndicator = UIActivityIndicatorView(style: .medium)
-    
-    // MARK: permanent data
-    private let currencyId: String
     
     private var isRequestInProgress: Bool = false {
         didSet {
@@ -27,29 +26,22 @@ final class DefaultCurrencyDetailsView: UIViewController {
         }
     }
     
-    init(currencyId: String) {
-        self.currencyId = currencyId
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) is not supported")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupLayout()
         setupAppearence()
+        
+        assert(presenter != nil)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        sendGroupRequests()
+        presenter?.requestCurrencyDetails()
     }
     
-    func updateView(model: CurrencyModel?, icon: UIImage?) {
+    private func updateView(model: CurrencyModel?, icon: UIImage?) {
         titleLabel.text = model?.name ?? "Invalid data"
         titleLabel.sizeToFit()
         
@@ -113,76 +105,21 @@ private extension DefaultCurrencyDetailsView {
         priceLabel.font = UIFont.systemFont(ofSize: 18)
         activityIndicator.hidesWhenStopped = true
     }
-    
-    // MARK: reqeasts
-    
-    func sendGroupRequests() {
-        isRequestInProgress = true
-        
-        var tmpModel: CurrencyModel?
-        var tmpImage: UIImage?
-        
-        let group = DispatchGroup()
-        group.enter()
-        requestDetails(id: currencyId) { model in
-            tmpModel = model
-            group.leave()
-        }
-        
-        group.enter()
-        requestIcon(id: currencyId) { image in
-            group.leave()
-            tmpImage = image
-        }
-        
-        group.notify(queue: DispatchQueue.main) {
-            self.updateView(model: tmpModel, icon: tmpImage)
-            self.isRequestInProgress = false
-        }
-    }
-      
-    func requestDetails(id: String, completion: @escaping (CurrencyModel?) -> ()) {
-        APIManager.shared.getCurrencyDetails(ids: id) { result in
-            var model: CurrencyModel? = nil
-            switch result {
-            case .success(let models):
-                model = models[0]
-            case .failure(let error):
-                print(error)
-            }
-            
-            completion(model)
-        }
+
+}
+
+// MARK: CurrencyDetailsView implementation
+
+extension DefaultCurrencyDetailsView: CurrencyDetailsView {
+    func setActivityIndicator(activated: Bool) {
+        isRequestInProgress = activated
     }
     
-    func requestIcon(id: String, completion: @escaping (UIImage?) -> ()) {
-        APIManager.shared.getCurrencyIcon { result in
-            let group = DispatchGroup()
-            var image: UIImage? = nil
-            
-            switch result {
-            case .success(let icons):
-                if let icon = icons.first(where: {$0.id == id}) {
-                    group.enter()
-                    APIManager.shared.downloadImage(url: icon.iconUrl) { result in
-                        switch result {
-                        case .success(let downloadedImage):
-                            image = downloadedImage
-                        case .failure(let error):
-                            print(error)
-                        }
-                        
-                        group.leave()
-                    }
-                }
-            case .failure(let error):
-                print(error)
-            }
-            
-            group.notify(queue: DispatchQueue.global()) {
-                completion(image)
-            }
-        }
+    func updateView() {
+        updateView(
+            model: presenter?.currencyModel,
+            icon: presenter?.currencyIcon
+        )
     }
 }
 
